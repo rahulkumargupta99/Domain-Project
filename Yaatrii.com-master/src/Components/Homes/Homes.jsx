0716807d512c2717feaedd097f4c18f5e3deaada
Rahul kumar <rahul8822.com@gmail.com>
2024-06-19 20:49:27 +0530

    Initial commit

---
+++ Yaatrii.com-master/src/Components/Homes/Homes.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import HomePage from '../HomePage/HomePage';
+import Packages from '../Packages/Packages';
+import Connect from '../Connect/Connect';
+import Footer from '../Footer/Footer';
+
+
+
+const Homes = () => {
+  return (
+   <div>
+    <HomePage/>
+    <Packages/>
+    <Connect/>
+    <Footer/>
+   </div>
+     
+  )
+}
+
+export default Homes
